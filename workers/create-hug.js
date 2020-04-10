5f9b27ffc9aa43260f601b35d8f49f6927f2cf90
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-04-10 21:35:23 +0200

    npmignore

--- workers/create-hug.js
+++ workers/create-hug.js
@@ -2,15 +2,30 @@ const cuid = require("cuid");
 
 const Hug = require("../site/src/pages/hug");
 
-function createHug(body) {
+async function createHug(body) {
   const hugHtml = Hug({
     senderName: body.sender_name,
     receiverName: body.receiver_name,
     additionalComments: body.additional_comments,
     hugType: body.hug_type
   });
+  const id = cuid();
 
-  
+  // Create dist folder if not existing
+  try {
+    await mkdir(path.join(__dirname, "../dist"));
+  } catch (ex) {
+    console.info("Dist dir already exists");
+  }
+
+  // Output index contents
+  try {
+    await writeFile(path.join(__dirname, `../dist/${id}/index.html`), hugHtml);
+  } catch (ex) {
+    console.error(ex);
+  }
+
+  return
 }
 
 module.exports = createHug;
