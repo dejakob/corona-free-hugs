b515e50ff9c42e57be1cb5b100483d99276fd2a7
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-04-10 15:20:15 +0200

    Initial commit

---
+++ site/src/components/head.js
@@ -0,0 +1,5 @@
+const React = require("react");
+
+function Head() {}
+
+module.exports = Head;
---
+++ site/src/pages/index.js
@@ -0,0 +1,24 @@
+const React = require("react");
+
+/**
+ *
+ */
+function IndexPage() {
+  return (
+    <>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>{title}</title>
+        </head>
+        <body></body>
+      </html>
+    </>
+  );
+}
+
+module.exports = IndexPage;
