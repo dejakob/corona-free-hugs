27c4840acd0a0eef773241fb74d4c764048f19fb
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-04-12 11:58:30 +0200

    Add hugs

--- site/src/config/hug-types.js
+++ site/src/config/hug-types.js
@@ -18,6 +18,31 @@ module.exports = [
     width: 360
   },
   {
+    image: "https://media.giphy.com/media/YT95XJOLvY1t2SJgpR/giphy.gif",
+    alt: "Hang in there",
+    title: "Hang in there",
+    content: "Don't give up! The crisis will end one day... maybe...",
+    height: 300,
+    width: 300
+  },
+  {
+    image: "https://media.giphy.com/media/NhdNJS36csweY/giphy.gif",
+    alt: "Birthday - Michael the office",
+    title: "COVID-19 birthday hug",
+    content:
+      "Birthdaying during the Corona crisis sucks, but here's a virtual hug to make you feel better!",
+    height: 300,
+    width: 400
+  },
+  {
+    image: "https://media.giphy.com/media/9M5jK4GXmD5o1irGrF/giphy.gif",
+    alt: "This is fine meme",
+    title: "This is fine",
+    content: "Everything is (going to be) fine",
+    height: 300,
+    width: 275
+  },
+  {
     image: "https://media.giphy.com/media/U0nW0QagDmTwA/giphy.gif",
     alt: "I request the highest of fives - Barney Stinson",
     title: "The highest of fives",
@@ -25,23 +50,6 @@ module.exports = [
       "Why give a hug if you can send a legen... WAIT FOR IT... dary high five?",
     height: 300,
     width: 600
-  },
-  {
-    image: "https://media.giphy.com/media/3oxHQDfHIGY7jYSbuw/giphy.gif",
-    alt: "Take it sleazy - Michael of the good place",
-    title: "Take it sleazy",
-    content:
-      "Remind your loved ones to take things easy while they're living in the bad place",
-    height: 300,
-    width: 600
-  },
-  {
-    image: "https://media.giphy.com/media/YT95XJOLvY1t2SJgpR/giphy.gif",
-    alt: "Hang in there",
-    title: "Hang in there",
-    content: "Don't give up! The crisis will end one day... maybe...",
-    height: 300,
-    width: 300
   },
   {
     image: "https://media.giphy.com/media/NhjPhBQIIxdxm/giphy.gif",
@@ -53,11 +61,12 @@ module.exports = [
     width: 232
   },
   {
-    image: "https://media.giphy.com/media/NhdNJS36csweY/giphy.gif",
-    alt: "Birthday - Michael the office",
-    title: "COVID-19 birthday hug",
-    content: "Birthdaying during the Corona crisis sucks, but here's a virtual hug to make you feel better!",
+    image: "https://media.giphy.com/media/3oxHQDfHIGY7jYSbuw/giphy.gif",
+    alt: "Take it sleazy - Michael of the good place",
+    title: "Take it sleazy",
+    content:
+      "Remind your loved ones to take things easy while they're living in the bad place",
     height: 300,
-    width: 400
+    width: 600
   }
 ];
