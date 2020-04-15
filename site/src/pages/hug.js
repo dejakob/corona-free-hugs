fd9bdff0916104735b68731a4e65a2542c5705cc
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-04-15 18:57:30 +0200

    copy to clipboard iOS

--- site/src/pages/hug.js
+++ site/src/pages/hug.js
@@ -120,10 +120,12 @@ function Hug({
             __html: `
             if (window.location.href.indexOf('created=true') > -1) { showNotification(); }
             if (typeof history !== 'undefined') history.replaceState({}, document.title, window.location.origin + window.location.pathname);
-            document.querySelector('.copyToClipboard').addEventListener('click', copyToClipboard);
+            var copyToClipboardBtn = document.querySelector('.copyToClipboard');
+            if (copyToClipboardBtn) copyToClipboardBtn.addEventListener('click', handleCopyToClipboard);
             function showNotification() { document.getElementById('successNotification').style.display = 'block'; }
             function hideNotification() { document.getElementById('successNotification').style.display = 'none'; }
-            function copyToClipboard() { try { navigator.clipboard.writeText(window.location.origin + window.location.pathname); hideNotification(); } catch (ex) {} }
+            function handleCopyToClipboard() { try { copyToClipboard(window.location.origin + window.location.pathname); } catch (ex) { alert('Could not access your clipboard. Please copy the full URL (on top of the page)'); } hideNotification(); }
+            function copyToClipboard(e){var n,t,o,a;t=e,(n=document.createElement("textArea")).readOnly=!0,n.contentEditable=!0,n.value=t,document.body.appendChild(n),navigator.userAgent.match(/ipad|iphone/i)?((o=document.createRange()).selectNodeContents(n),(a=window.getSelection()).removeAllRanges(),a.addRange(o),n.setSelectionRange(0,999999)):n.select(),document.execCommand("copy"),document.body.removeChild(n)}
         `
           }}
         />
