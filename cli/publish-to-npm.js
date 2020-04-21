4ecc7186fd218e8fffe5fe0fdc476e2cca41fd81
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-04-21 22:18:51 +0200

    git

--- cli/publish-to-npm.js
+++ cli/publish-to-npm.js
@@ -1,8 +1,10 @@
-const { execSync } = require('child_process');
+const { execSync } = require("child_process");
 
 const npmRc = `https://registry.npmjs.org/:_authToken=${process.env._NPM_TOKEN}\nregistry=https://registry.npm`;
 
-console.log('npm rc', npmRc)
+console.log("npm rc", npmRc);
 
 execSync(`echo "${npmRc}" > .npmrc`);
-execSync(`export NPM_TOKEN=${process.env._NPM_TOKEN} npm publish`);
+execSync(`npm publish`);
+
+console.log("published");
