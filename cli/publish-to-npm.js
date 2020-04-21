7dc08c90d9284dcdf1093a27c373bd29f9d0e217
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-04-21 21:54:59 +0200

    git

--- cli/publish-to-npm.js
+++ cli/publish-to-npm.js
@@ -1,3 +1,3 @@
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 
-exec(`NPM_TOKEN=${process.env._NPM_TOKEN} npm publish`);
+execSync(`NPM_TOKEN=${process.env._NPM_TOKEN} npm publish`);
