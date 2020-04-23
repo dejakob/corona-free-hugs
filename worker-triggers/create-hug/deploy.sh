c66f645650ce8931281716458cf009c17d898616
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-04-23 20:42:34 +0200

    Set project id function deploy

--- worker-triggers/create-hug/deploy.sh
+++ worker-triggers/create-hug/deploy.sh
@@ -1,1 +1,1 @@
-gcloud functions deploy create-hug --runtime nodejs10 --trigger-http
+gcloud functions deploy create-hug --runtime nodejs10 --trigger-http --project $(node -p "require('../../credentials.js').project_id")
