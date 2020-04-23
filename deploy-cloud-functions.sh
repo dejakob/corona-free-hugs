29be8bcbe2a0510324c3802549a4aa879fae2fe9
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-04-23 21:05:03 +0200

    add credentials.json

--- deploy-cloud-functions.sh
+++ deploy-cloud-functions.sh
@@ -1,4 +1,4 @@
-gcloud auth activate-service-account --key-file ./credentials.js
+gcloud auth activate-service-account --key-file ./credentials.json
 gcloud config set account $(node -p "require('./credentials.js').account")
 
 cd ./worker-triggers/create-hug && ./deploy.sh
