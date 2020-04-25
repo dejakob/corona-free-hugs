daf8ccd10e1efc8497ab92da57b9dcff170ec91d
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-04-25 18:52:37 +0200

    always install latest version of package for cloud function

--- worker-triggers/create-hug/deploy.sh
+++ worker-triggers/create-hug/deploy.sh
@@ -1,1 +1,2 @@
+npm i --save --save-exact corona-free-hugs@latest
 gcloud functions deploy create-hug --runtime nodejs10 --trigger-http --project $(node -p "require('../../credentials.js').project_id")
